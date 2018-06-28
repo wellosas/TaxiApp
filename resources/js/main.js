0cff523c5b6182bf5244a7a3c865d9e856dae759
Wellington Oronsaye <OronsW@Wellington-Oronsayes-iMac.local>
2018-06-28 15:34:06 -0230

    to be redifined

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,2 @@
 (() => {
-    var page = document.querySelector('.page'),
-    pageWidth = page.offsetWidth,
-    pageHeight = page.offsetHeight;
-    console.log('width', pageWidth, 'height', pageHeight);
-
 })();
