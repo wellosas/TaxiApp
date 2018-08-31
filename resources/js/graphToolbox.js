09062514f1871bd3d41515edbb4c3d836846c747
Wellington Oronsaye <OronsW@Wellington-Oronsayes-iMac.local>
2018-08-30 21:50:23 -0230

    created graphToolbox.js file

---
+++ resources/js/graphToolbox.js
@@ -0,0 +1,14 @@
+define([
+    'lib/d3/d3'
+], function(d3) {
+    'use strict';
+    return {
+        margin: {
+            top : 25,
+            bottom : 25,
+            left : 25,
+            right : 25
+        }
+    }
+    
+});
